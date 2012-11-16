05f6c34567da3c46d6d1d839e1eb770b41cdd546
tdeboer <deboer.t@gmail.com>
2012-11-16 09:54:01 +0100

    sass cleanup

--- scripts/jquery.ui.pageswipe.js
+++ scripts/jquery.ui.pageswipe.js
@@ -2,50 +2,57 @@ define(['jqui'], function() {
 	
 	
 	
-	(function($) {
-
-	    $.widget("ui.pageswipe", {
-			options: {
-				location: "bottom",
-				color: "#fff",
-				backgroundColor: "#000"
-			},
-					
-			_create: function() {
+	(function( $ ){
+	
+		var methods = {
+			init : function( options ) {
 				
-				var self = this,
-					o = self.options,
-					el = self.element,
-					
-				
-				el.bind('touchstart', this._touchStart);
-				el.bind('touchend', this._touchEnd);
-				el.bind('touchmove', this._touchMove);
+				/*
+				left
+				position: absolute
+				*/
+	
+				return this.each(function(index){
+					$(this).css('position', 'absolute');
+					var left = index * $('.wrapper').width();
+					$(this).css('left', left+'px');
+					$(window).bind('resize.tooltip', methods.reposition);
+				});
+	
 			},
-			
-			_touchStart: function() {
-				$('section:first').text(e.originalEvent.touches[0].clientX);
-			},
-			_touchEnd: function() {
-
-				
-			},
-			_touchMove: function(e) {
-					
-				
-			},
-					
-			destroy: function() {			
-				this.element.next().remove();
-			},
-			
-			_setOption: function(option, value) {
-				$.Widget.prototype._setOption.apply( this, arguments );
-				
+			destroy : function( ) {
+	
+				return this.each(function(){
+					$(window).unbind('.tooltip');
+				})
 			}
-		});
-	})(jQuery);
-
+		};
+	
+		$.fn.pageswipe = function( method ) {
+	    
+			if ( methods[method] ) {
+				return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+			} else if ( typeof method === 'object' || ! method ) {
+				return methods.init.apply( this, arguments );
+			} else {
+				$.error( 'Method ' +  method + ' does not exist on jQuery.pageswipe' );
+			}
+			
+			/* 
+				bind
+					touch events
+					window resize
+				
+				methods
+					loadNext
+					touchMove
+						swipeNext
+						swipeBack
+				*/
+	  
+		};
+	
+	})( jQuery );
 	
 	
 	
